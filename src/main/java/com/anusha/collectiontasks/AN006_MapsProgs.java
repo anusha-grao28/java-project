04c7f89f8902d60f76cb756e90d69806f528cbd2
anusha <anusha.grao28@gmail.com>
2019-02-12 19:34:48 +0530

    working  on  collection  set and  maps  progs

--- src/main/java/com/anusha/collectiontasks/AN006_MapsProgs.java
+++ src/main/java/com/anusha/collectiontasks/AN006_MapsProgs.java
@@ -3,6 +3,7 @@ package com.anusha.collectiontasks;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Map;
 
 public class AN006_MapsProgs {
     public static void main(String[] args){
@@ -16,12 +17,23 @@ public class AN006_MapsProgs {
         hmap.put(3,"Gopal");
         hmap.put(4,"sriraksha");
 
-        // to display the contents of Map
+        //2. to display the contents of Map
 
         //1. method-I
         System.out.println("Method-I for displaying map contents: \n"+Arrays.asList(hmap));
-
+        System.out.println("----------------\n");
         //2. method-II
         System.out.println("Method-II for displaying map contents: \n"+ Collections.singletonList(hmap));
+        System.out.println("----------------\n");
+        //3. Method-III
+        for (Map.Entry<Integer, String> entry : hmap.entrySet()) {
+            System.out.println(entry.getKey()+" : "+entry.getValue());
+        }
+        System.out.println("----------------\n");
+        //3. to print the third element of the map
+        System.out.println(" the elements of the map is: "+hmap.size());
+        System.out.println("the third element from the map is: "+hmap.get(3));
+
+
     }
 }
